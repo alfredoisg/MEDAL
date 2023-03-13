2b2cc5fa274cf8721f50a27baf04252e8b7b77fb
alfredoisg <alfredo.sanchez@sintef.no>
2023-03-13 16:44:08 +0100

    cleaned frames

--- GUI/frames.py
+++ GUI/frames.py
@@ -32,37 +32,6 @@ class MainMenuFrame(ctk.CTkFrame):
     def hide(self):
         self.pack_forget()
 
-class MainMenuFrame2(ctk.CTkFrame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        
-        self.parent = parent
-        
-        self.label = ctk.CTkLabel(self, text="Main menu")
-        self.label.grid(row=0, column=0, pady=10)
-        
-        self.plot_button = ctk.CTkButton(self, text="Create Plot", command=self.on_plot_button)
-        self.plot_button.grid(row=1, column=0, pady=10)
-        
-        self.calculate_button = ctk.CTkButton(self, text="Calculate", command=self.on_calculate_button)
-        self.calculate_button.grid(row=2, column=0, pady=10)
-        
-        self.exit_button = ctk.CTkButton(self, text="Exit", command=self.quit)
-        self.exit_button.grid(row=3, column=0, pady=10)
-        
-    def on_plot_button(self):
-        self.parent.go_to_frame("example")
-        
-    def on_calculate_button(self):
-        # TODO: implement calculation and display result in textbox
-        pass
-        
-    def show(self):
-        self.grid(row=0, column=1, sticky="nsew")
-        
-    def hide(self):
-        self.grid_forget()
-
 class ExampleFrame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
